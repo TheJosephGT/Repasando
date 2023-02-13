06ffec4793bb27d5631a2061659dc4e937ec8583
JosephGT <joseph2002610@gmail.com>
2023-02-13 14:26:44 -0400

    Creamos el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+
+public class Contexto : DbContext {
+
+    public DbSet<Libros> Libros {get; set;}
+
+    public Contexto (DbContextOptions<Contexto> options): base(options){}
+}
